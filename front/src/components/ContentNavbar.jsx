c4c0f4789efa59c1d943d2f01930d192caee5177
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-03-13 17:55:36 -0500

    Mejorando el navbar

--- front/src/components/ContentNavbar.jsx
+++ front/src/components/ContentNavbar.jsx
@@ -1,6 +1,7 @@
 export default function ContentNavbar ({ children, hideShow }) {
+  const hidden = hideShow ? 'translate-x-full' : 'translate-x-0'
   return (
-    <div className={`flex lg:transition-none lg:translate-x-0 lg:flex-row lg:gap-1 lg:items-center lg:static lg:bg-none lg:min-w-0 lg:min-h-0 fixed min-w-full bg-primary-blue min-h-screen top-0 left-0 flex-col justify-center px-1 transition-all gap-4 ${hideShow}`}>
+    <div className={`flex md:transition-none md:translate-x-0 md:flex-row md:gap-1 md:items-center md:static md:bg-none md:min-w-0 md:min-h-0 fixed min-w-full bg-primary-blue min-h-screen top-0 left-0 flex-col justify-center px-1 transition-all gap-4 ${hidden}`}>
       {children}
     </div>
   )
---
+++ front/src/components/LogedNavbar.jsx
@@ -0,0 +1,66 @@
+import {
+  IconHome,
+  IconUser,
+  IconFileInvoice,
+  IconAccessible
+} from '@tabler/icons-react'
+import { NavLink } from 'react-router-dom'
+import MyAvatar from './MyAvatar'
+
+const linksRouter = [
+  {
+    id: 'link1',
+    name: 'Inicio',
+    icon: <IconHome stroke='2' size={25} />,
+    path: '/',
+    verify: true
+  },
+  {
+    id: 'link2',
+    name: 'Valoración unitaria',
+    icon: <IconUser stroke='2' size={25} />,
+    path: '/valoracion',
+    verify: true
+  },
+  {
+    id: 'link3',
+    name: 'Subir archivo',
+    icon: <IconFileInvoice stroke='2' size={25} />,
+    path: '/archivo',
+    verify: false
+  },
+  {
+    id: 'link4',
+    name: 'Unidades',
+    icon: <IconAccessible stroke='2' size={25} />,
+    path: '/unidades',
+    verify: false
+  }
+]
+
+const Links = () => {
+  const stylesDefault = 'flex flex-col items-center h-full py-1 px-3 rounded-2xl transition-all duration-400 ease-in-out text-white font-medium text-xl md:text-base'
+  const stylesActive = `${stylesDefault.replace('text-white', 'text-primary-blue')} bg-white font-bolder`
+  return linksRouter.map(link => {
+    const fd = ''
+    return (
+      <NavLink
+        key={link.id}
+        to={link.path}
+        className={({ isActive }) => { return isActive ? stylesActive : stylesDefault }}
+        onClick={() => console.log('click')}
+      >
+        {({ isActive }) => isActive ? <>{link.icon} <p>{link.name}</p></> : <>{link.icon} <p className='absolute opacity-0'>{link.name}</p></>}
+      </NavLink>
+    )
+  })
+}
+
+export default function LogedNavbar () {
+  return (
+    <>
+      <Links />
+      <MyAvatar fullName='Brayan Villamizar' className='self-center cursor-pointer md:ml-2' />
+    </>
+  )
+}
