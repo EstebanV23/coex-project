ba71b94c177ffae349c922b4e98ae26ad639c98f
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-03-14 15:46:04 -0500

    Arreglando el eslint en los componentes y el back

--- back/src/routes/authentication/authRoute.js
+++ back/src/routes/authentication/authRoute.js
@@ -1,41 +1,41 @@
-import express from "express";
-import NutritionistController from "../../controllers/NutitionistController.js";
-import handlerJwtVerify from "../../middlewares/handlerJwtValidate.js";
-import validateData from "../../middlewares/validationData.js";
+import express from 'express'
+import NutritionistController from '../../controllers/NutitionistController.js'
+import handlerJwtVerify from '../../middlewares/handlerJwtValidate.js'
+import validateData from '../../middlewares/validationData.js'
 import {
   nutritionistLogin,
   nutritionistRegister,
   nutritionistForgotPassword,
-  nutritionistResetPassword,
-} from "../../schemas/requestSchema/nutritionistRequest.js";
+  nutritionistResetPassword
+} from '../../schemas/requestSchema/nutritionistRequest.js'
 
-const authRouter = express.Router();
+const authRouter = express.Router()
 
 authRouter
   .post(
-    "/signin",
-    validateData(nutritionistLogin, "body"),
+    '/signin',
+    validateData(nutritionistLogin, 'body'),
     NutritionistController.loginNutritionist
   )
   .post(
-    "/signup",
-    validateData(nutritionistRegister, "body"),
+    '/signup',
+    validateData(nutritionistRegister, 'body'),
     NutritionistController.createNutritionist
   )
   .get(
-    "/validate/:token",
-    handlerJwtVerify("params"),
+    '/validate/:token',
+    handlerJwtVerify('params'),
     NutritionistController.updateVerify
   )
   .post(
-    "/forgot-password",
-    validateData(nutritionistForgotPassword, "body"),
+    '/forgot-password',
+    validateData(nutritionistForgotPassword, 'body'),
     NutritionistController.forgotPassword
   )
   .post(
-    "/forgot-password/:token",
-    handlerJwtVerify("params"),
-    validateData(nutritionistResetPassword, "body"),
+    '/forgot-password/:token',
+    handlerJwtVerify('params'),
+    validateData(nutritionistResetPassword, 'body'),
     NutritionistController.resetPassword
-  );
-export default authRouter;
+  )
+export default authRouter
--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -1,73 +1,73 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-export default function Login() {
-  const URL = "http://localhost:5000/auth/signin";
+export default function Login () {
+  const URL = 'http://localhost:5000/auth/signin'
   const [login, setlogin] = useState({
-    email: "",
-    password: "",
-  });
+    email: '',
+    password: ''
+  })
   const loginChange = ({ target }) => {
-    const { name, value } = target;
+    const { name, value } = target
     setlogin({
       ...login,
-      [name]: value,
-    });
-  };
+      [name]: value
+    })
+  }
   const submitLogin = () => {
-    console.log(login);
+    console.log(login)
     const options = {
-      method: "POST",
-      headers: { "Content-type": "application/json;charset=UTF-8" },
-      body: `{"email":"${login.email}", "password":"${login.password}"}`,
-    };
-    console.log(options.body);
+      method: 'POST',
+      headers: { 'Content-type': 'application/json;charset=UTF-8' },
+      body: `{"email":"${login.email}", "password":"${login.password}"}`
+    }
+    console.log(options.body)
     fetch(`${URL}`, options)
       .then((response) => response.json())
       .then((response) => console.log(response))
-      .catch((err) => console.error(err));
-  };
+      .catch((err) => console.error(err))
+  }
   return (
-    <div className="flex justify-center items-center h-screen  bg-[#99c3c8]">
-      <div className="bg-white h-2/3 rounded-xl  w-11/12 lg:w-1/2 ">
-        <div className="container_title flex justify-center items-center mt-10">
-          <img src="logoFlor.svg" className="h-10" />
+    <div className='flex justify-center items-center h-screen  bg-[#99c3c8]'>
+      <div className='bg-white h-2/3 rounded-xl  w-11/12 lg:w-1/2 '>
+        <div className='container_title flex justify-center items-center mt-10'>
+          <img src='logoFlor.svg' className='h-10' />
           <strong>
-            <h1 className="text-center text-6xl font-work mt-2">mianthro</h1>
+            <h1 className='text-center text-6xl font-work mt-2'>mianthro</h1>
           </strong>
         </div>
 
-        <h1 className="text-center text-2xl mt-10">Iniciar sesión</h1>
+        <h1 className='text-center text-2xl mt-10'>Iniciar sesión</h1>
 
-        <div className="flex justify-end items-start w-full h-5/6 mt-10">
-          <div className="grid w-5/6">
-            <label htmlFor="" className="">
+        <div className='flex justify-end items-start w-full h-5/6 mt-10'>
+          <div className='grid w-5/6'>
+            <label htmlFor='' className=''>
               Correo electronico
             </label>
             <input
-              type="email"
-              placeholder="micorreo@ejemplo.com"
+              type='email'
+              placeholder='micorreo@ejemplo.com'
               required
-              className="w-10/12 h-8 mb-6 border-2 border-gray-500 rounded "
+              className='w-10/12 h-8 mb-6 border-2 border-gray-500 rounded '
               value={login.email}
-              name="email"
+              name='email'
               onChange={loginChange}
             />
 
-            <label htmlFor="" className="">
+            <label htmlFor='' className=''>
               Contraseña
             </label>
             <input
-              type="password"
-              id=""
-              placeholder="************"
+              type='password'
+              id=''
+              placeholder='************'
               required
-              className="w-10/12 h-8 mb-6  border-2 border-gray-500 rounded"
-              name="password"
+              className='w-10/12 h-8 mb-6  border-2 border-gray-500 rounded'
+              name='password'
               value={login.password}
               onChange={loginChange}
             />
             <button
-              className="bg-[#66a7ad] text-white h-8 w-10/12 rounded-md   hover:bg-[#3A676B]"
+              className='bg-[#66a7ad] text-white h-8 w-10/12 rounded-md   hover:bg-[#3A676B]'
               onClick={submitLogin}
             >
               Iniciar sesión
@@ -76,5 +76,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-  );
+  )
 }
