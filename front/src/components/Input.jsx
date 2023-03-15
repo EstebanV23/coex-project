db3d359b74fbd5c5dabb5c92e79c93623eb0039d
RABrL <robin_brito@hotmail.com>
2023-03-14 23:03:39 -0500

    Arregle el login y termine el input component

--- front/src/components/Input.jsx
+++ front/src/components/Input.jsx
@@ -1,11 +1,11 @@
 export default function Input ({ icon, ...props }) {
   return (
-    <div className='mx-auto w-screen-sm'>
-      <div className='relative flex h-10 w-full flex-row-reverse overflow-clip rounded-lg'>
-        <input className='peer w-full rounded-r-lg border border-slate-400 px-2 text-slate-900 placeholder-slate-400 transition-colors duration-300 focus:border-sky-400 focus:outline-none' {...props} />
-        <label className='flex items-center rounded-l-lg border border-slate-400 bg-slate-50 px-2 text-sm text-slate-400 transition-colors duration-300 peer-focus:border-sky-400 peer-focus:bg-primary-blue peer-focus:text-white' htmlFor='domain'>{icon}</label>
+    <div className='w-full'>
+      <div className='flex h-10 flex-row-reverse overflow-clip rounded-lg'>
+        <input className='peer w-full rounded-r-lg border border-slate-400 px-2 text-slate-900 placeholder-slate-400 transition-colors duration-300 focus:border-primary-blue focus:outline-none' {...props} />
+        <label className='flex items-center rounded-l-lg border border-slate-400 bg-slate-50 px-2 text-sm text-slate-400 transition-colors duration-300 peer-focus:border-primary-blue peer-focus:bg-primary-blue peer-focus:text-white' htmlFor='domain'>{icon}</label>
       </div>
-      <p className='text-error-600'> sdasda</p>
+      <p className='text-error-600 font-bold mt-1'>Usuario o contraseña incorrectas</p>
     </div>
   )
 }
