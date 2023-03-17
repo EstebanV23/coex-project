e7c93f9faa82178795cc28e8bc5d27cb7cdc476a
joker120620 <davidtoloza1234@hotmail.com>
2023-03-17 15:09:41 -0500

    FIN DEL RESPONSIVE: Pull request development

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -14,12 +14,12 @@ export default function Footer () {
           <p>Somos un grupo de desarrolladores que ha implementado esta herramienta para la ayuda de nutricionistas.</p>
           <img src='' alt='' />
 
-          <div className='my-5 w-[300px] h-[30px]'>
+          <div className='my-5 w-[200px] h-[20px] sm:w-[300px] sm:h-[30px]'>
             <img src='logoAzul.svg' />
           </div>
         </div>
 
-        <div className='w-80  '>
+        <div className='w-80 text-left '>
           <h3 className='text-xl font-bold text-white mb-2.5'>Navegacion</h3>
           <div className='flex flex-col  '>
             <NavLink to='/'>
