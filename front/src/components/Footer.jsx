616ad8f925da56c314a868fcfa4270ac534bc0b0
joker120620 <davidtoloza1234@hotmail.com>
2023-03-17 17:12:13 -0500

    xd

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-// import Modal from './ModalContact'
+import sendDataContact from './../services/modalEmailService'
 export default function Footer () {
   return (
     <>
       <div className='mt-10 md:mt-0 h-full '>
         <img src='wave.svg' width='100%' height='100%' />
       </div>
-      <div className='w-full h-fit flex flex-wrap 2xl:justify-around md:justify-start justify-center gap-10 p-7 font-light text-gray-500 text-base bg-black bottom-0'>
+      <div className='w-full h-fit flex flex-wrap 2xl:justify-around md:justify-start sm:justify-start justify-center gap-10 p-7 font-light text-gray-500 text-base bg-black bottom-0'>
 
         <div className='w-80 text-left'>
           <h3 className='text-xl font-bold text-white mb-2.5'>Nosotros</h3>
@@ -41,6 +41,7 @@ export default function Footer () {
         <div className='w-80 text-justify'>
           <h3 className='text-xl font-bold text-white mb-2.5'>Contacto</h3>
           <p>¿Quieres contactar con nosotros para futuros trabajos? Dejanos tu correo electronico y estaremos contactando contigo las proximas horas.</p>
+          <button onClick={sendDataContact} className='bg-blue-500 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>Contactar</button>
         </div>
       </div>
     </>
