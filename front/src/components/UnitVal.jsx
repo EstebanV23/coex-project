6168e7e0cd7698921f69c934cbf6be2d2aeb951c
Luisa <luisachalarca@gmail.com>
2023-03-20 19:27:10 -0500

    vista valoracion unitaria

--- front/src/components/UnitVal.jsx
+++ front/src/components/UnitVal.jsx
@@ -1,108 +1,61 @@
 import { Formik } from 'formik'
+import { regex } from '../constants/regex'
+import Button from './Button'
+import { LogoIcon } from './Icons'
+import Input from './Input'
 import SelectUnit from './Select'
+import { GiBodyHeight, FaWeight, GrBaby } from 'react-icons/gi'
+import { useState } from 'react'
+import Navbar from './Navbar'
 
 export default function UnitVal () {
+  const loading = useState(false)
+
   return (
-    <Formik
-      initialValues={{
-        peso: '',
-        talla: '',
-        genero: ''
-      }}
-      validate={(values) => {
-        const errores = {}
-        if (!values.peso) {
-          errores.peso = '*campo obligatorio'
-        }
-        if (!values.talla) {
-          errores.talla = '*campo obligatorio'
-        }
-        return errores
-      }}
-      onSubmit={(values) =>
-        console.log(values)}
-    >
-      {({ errors, values, handleSubmit, handleChange, handleBlur, touched }) => (
-        <form className='flex justify-center items-center h-screen  bg-[#99c3c8]' onSubmit={handleSubmit}>
-          <div className='bg-white h-2/3 rounded-xl  w-11/12 lg:w-1/2 '>
-            <div className='container_title flex justify-center items-center mt-10'>
-              <img src='logoFlor.svg' className='h-10' />
-              <strong>
-                <h1 className='text-center text-6xl font-work mt-2'>mianthro</h1>
-              </strong>
-            </div>
-
-            <h1 className='text-center text-2xl mt-10'>Valoración unitaria</h1>
-
-            <div className='flex justify-end items-start w-full h-5/6 mt-10'>
-              <div className='grid w-5/6'>
-                <div className='mb-6'>
-                  <label htmlFor='' className=''>
-                    Peso
-                  </label>
-                  <input
-                    type='number'
-                    placeholder='12'
-                    required
-                    className='w-10/12 h-8  border-2 border-gray-500 rounded '
-                    name='peso'
-                    value={values.peso}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {touched.peso && errors.peso ? <div className='text-error-700'>{errors.peso}</div> : null}
-                </div>
-
-                <div className='mb-6'>
-                  <label htmlFor='' className=''>
-                    Talla
-                  </label>
-                  <input
-                    type='number'
-                    id=''
-                    placeholder='10'
-                    required
-                    className='w-10/12 h-8  border-2 border-gray-500 rounded'
-                    name='talla'
-                    value={values.talla}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {touched.talla && errors.talla ? <div className='text-error-700'>{errors.talla}</div> : null}
-                </div>
-                <div className='mb-6'>
-                  <label htmlFor=''>Género</label>
-                  <SelectUnit />
-                </div>
-                <div className='mb-6'>
-                  <label htmlFor='' className=''>
-                    Valoracón
-                  </label>
-                  <input
-                    type='text'
-                    id=''
-                    placeholder='5'
-                    required
-                    disabled
-                    className='w-10/12 h-8  border-2 border-gray-500 rounded'
-                    name='talla'
-                    value={values.valoracion}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {touched.valoracion && errors.valoracion ? <div className='text-error-700'>{errors.valoracion}</div> : null}
-                </div>
-                <button
-                  type='submit'
-                  className='bg-[#66a7ad] text-white h-8 w-10/12 rounded-md   hover:bg-[#3A676B]'
-                >
-                  Valorar
-                </button>
+    <>
+      <Navbar />
+      <Formik
+        initialValues={{
+          height: '',
+          weight: '',
+          gender: ''
+        }}
+      >
+        {({ errors, handleSubmit }) => (
+          <div className='flex justify-center items-center'>
+            <form className='bg-white h-fit p-3 rounded-xl w-[95%] sm:p-6 md:p-12 md:max-w-3xl lg:w-3xl' onSubmit={handleSubmit}>
+              <div className='flex flex-col gap-0 sm:gap-4 mb-5 justify-center items-center'>
+                <LogoIcon fill='black' />
+                <h1 className='text-3xl font-work font-bold'>Registro</h1>
               </div>
-            </div>
+              <div className=' flex flex-wrap gap-6 w-full'>
+
+                <div className='flex flex-col md:flex-row gap-6 w-full'>
+                  <Input
+                    disabled={loading} icon={<GiBodyHeight size={22} />} type='text' placeholder='Talla' name='height' error={errors}
+                  />
+                  <Input
+                    disabled={loading} icon={<FaWeight size={22} />} type='text' placeholder='Peso' name='weight' error={errors}
+                  />
+                </div>
+                <div className='flex flex-col md:flex-row gap-6 w-full'>
+                  <SelectUnit
+                    disabled={loading} icon={<GrBaby size={22} />} type='text' placeholder='Género' name='gender' error={errors}
+                  />
+                </div>
+                <div className='flex flex-col md:flex-row gap-6 w-full'>
+                  <Input
+                    disabled={loading} type='text' placeholder='Valoración' name='valoracion' error={errors}
+                  />
+                </div>
+                <Button disabled={loading} type='submit' className='py-2 transition-all duration-500 text-xl text-primary-blue font-bold hover:bg-primary-blue hover:text-white border-primary-blue'>Valorar</Button>
+
+              </div>
+            </form>
           </div>
-        </form>
-      )}
-    </Formik>
+
+        )}
+      </Formik>
+    </>
   )
 }
