3c38aa4915379afc2730527bcd9d5e1570b04ddd
Luisa <luisachalarca@gmail.com>
2023-03-21 20:27:36 -0500

    organizando typo en el diccionario

--- front/src/constants/valorationDictionary.js
+++ front/src/constants/valorationDictionary.js
@@ -2,7 +2,7 @@ export const valoracionDiccionario = {
   0: 'Peso adecuado para la talla',
   1: 'Riesgo de desnutrición',
   2: 'Desnutrición aguda moderada',
-  3: 'Desnutricion aguda severa',
+  3: 'Desnutrición aguda severa',
   4: 'Obesidad',
   5: 'Riesgo de sobrepeso',
   6: 'Sobrepeso'
