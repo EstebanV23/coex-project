df38c9042f503321a0f6bf68c3de88a7b8683e57
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-03-28 17:51:47 -0500

    Arreglando lo sestilos

--- front/src/components/LinksNavbar.jsx
+++ front/src/components/LinksNavbar.jsx
@@ -6,7 +6,7 @@ import sweetAlert from './../constants/sweetAlert'
 
 export default function LinksNavbar () {
   const { openLoggin } = useModalStore(store => store, shallow)
-  const stylesDefault = 'flex flex-col items-center h-full pt-1 px-3 rounded-2xl transition-all duration-300 ease-in-out text-white text-opacity-80 hover:text-opacity-100 font-medium text-lg md:text-base hover:bg-primary-blue-700 hover:font-bolder hover:bg-opacity-30'
+  const stylesDefault = 'flex flex-col items-center h-full pt-1 px-3 rounded-2xl transition-all duration-300 ease-in-out text-white text-opacity-90 hover:text-opacity-100 font-medium text-lg md:text-base hover:bg-primary-blue-700 hover:font-bolder hover:bg-opacity-30'
   const stylesActive = `${stylesDefault} bg-primary-blue-700 bg-opacity-50 font-bolder text-opacity-100`
 
   function handleClickVerify (e) {
